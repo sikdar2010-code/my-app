20a1802fb6cfb040bc67e1149f4f46d27459ebcf
Soumik Sikdar <sikdar2010@gmail.com>
2026-03-22 01:38:33 +0530

    initial commit

--- frontend/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }) {
-  return (
-    <div className="bg-white shadow-lg p-6 rounded-xl w-96">
-      {children}
-    </div>
-  );
-}
--- frontend/lib/api/auth.js
+++
@@ -1,8 +0,0 @@
-// lib/api/auth.js
-export const loginUser = async (data) => {
-  const res = await fetch("/api/login", {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
-  return res.json();
-};
